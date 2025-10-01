563bad4fed2123e9a2deaca0fb02d5a254c73758
Seasawher <tellagoodstory@icloud.com>
2025-10-01 22:39:43 +0900

    test for input

--- main.ts
+++ main.ts
@@ -11,8 +11,10 @@ function getLatestRelease (stable : boolean) : string {
 
 function run() {
   const legacy_update = core.getInput("legacy_update");
+  const directory = core.getInput("lake_package_directory");
   core.info("Hello World");
-  core.info(`legacy_update: ${legacy_update}`)
+  core.info(`legacy_update: ${legacy_update}`);
+  core.info(`directory: ${directory}`);
 }
 
 run();
